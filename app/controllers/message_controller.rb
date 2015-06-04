3643369714a144ec12b1e5765614f61e73e74aa8
John Daugherty <johndaugherty1986@gmail.com>
2015-06-04 16:02:52 -0400

    another commit

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,2 +1,15 @@
 class MessageController < ApplicationController
+  def index
+    render json: Message.all
+  end
+
+    def create
+    begin
+      newmessage = Message.create(text: params.fetch(:text),name: params.feth(:name))
+      render json: todolist
+    rescue ActionController::ParameterMissing => error
+      render json: { error: error.message }, status: 422
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "main", to: "message#index"
+  post "main", to: "message#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
