f6fd8561698544d8462d4fce1419b41297f52c12
John Daugherty <johndaugherty1986@gmail.com>
2015-06-04 15:10:59 -0400

    initial commit

---
+++ db/migrate/20150604190720_create_message.rb
@@ -0,0 +1,11 @@
+class CreateMessage < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :name, null: false
+      t.string :room, default: "Main"
+      t.string :text, limit: 300, null: false
+      t.timestamps null: false
+      t.boolean :status, default: true
+    end
+  end
+end
