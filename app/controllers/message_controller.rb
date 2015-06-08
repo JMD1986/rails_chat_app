c9a980876766e686f325b3f8ca276582ea4feac3
John Daugherty <johndaugherty1986@gmail.com>
2015-06-07 20:41:24 -0500

    testing bot

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -85,19 +85,19 @@ class MessageController < ApplicationController
   end
 
 
-  def bot_response
-    bot_reply = Message.new
-    case params.fetch(:text)
-    when "salad"
-      bot_reply.name = "Chatbot"
-      bot_reply.text = "I love salads"
-      bot_reply.save
-    when "help me"
-      bot_reply.name = "Chatbot"
-      bot_reply.text = "I only help those who help themselves"
-      bot_reply.save
-    end
-  end
+  # def bot_response
+  #   bot_reply = Message.new
+  #   case params.fetch(:text)
+  #   when "salad"
+  #     bot_reply.name = "Chatbot"
+  #     bot_reply.text = "I love salads"
+  #     bot_reply.save
+  #   when "help me"
+  #     bot_reply.name = "Chatbot"
+  #     bot_reply.text = "I only help those who help themselves"
+  #     bot_reply.save
+  #   end
+  # end
 
   def profile
     begin
