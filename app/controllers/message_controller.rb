8534274002bccfb4263aa58728baccf422e23d29
John Daugherty <johndaugherty1986@gmail.com>
2015-06-07 20:50:58 -0500

    reverting to older style

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -6,90 +6,65 @@ class MessageController < ApplicationController
     render json: $all_posts
   end
 
-  #version for multiple rooms
-  # def create
-  #   begin
-  #     newmessage = Message.create(text: params.fetch(:text),
-  #                                 name: params.fetch(:name),
-  #                                 room: params.fetch(:room))
-  #     render json: newmessage
-  #   rescue ActionController::ParameterMissing => error
-  #     render json: {error: error.message }, status: 422
-  #   end
-  # end
-
-  #version for only one room
-   def create
+  def create
     begin
       newmessage = Message.create(text: params.fetch(:text),
-                                  name: params.fetch(:name),)
+                                  name: params.fetch(:name),
+                                  room: params.fetch(:room))
       render json: newmessage
     rescue ActionController::ParameterMissing => error
       render json: {error: error.message }, status: 422
     end
   end
 
-  #
   def recent_messages
-    begin
-      recent_messages = Message.where(room: params[:room])
-                               .select {|row| row.created_at > (Time.now - 300) }
-      render json: recent_messages
-    rescue ActionController::ParameterMissing => error
-      render json: {error: error.message }, status: 422
-    end
+    recent_messages = Message.where(room: params[:room])
+                             .select {|row| row.created_at > (Time.now - 300) }
+    render json: recent_messages
   end
+
+# will work on
+#   def chat_bot
+#     $all_posts.each do |row|
+#       if row.text.contains?("chatbot")
+#         Message.create(text: "It is #{Time.now}", name: "Chatbot", room: "main")
+# end
+#   end
 
   def recent_users
     begin
       recent_user_data = $all_posts.select { |row| row.created_at > (Time.now - 14400)}
-      render json: recent_user_data
-    rescue ActionController::ParameterMissing => error
-      render json: { error: error.message }, status: 422
+        render json: recent_user_data
+      rescue ActionController::ParameterMissing => error
+        render json: { error: error.message }, status: 422
+      end
     end
-  end
 
   def top_ten_users
-    begin
-      top_users = $all_posts.group_by { |row| row.name }
-                            .sort_by { |key, value| value.count}
-                            .map{ |row| {name: row.first, count: row.last.count}}
-      render json: top_users.reverse.take(10)
-    rescue ActionController::ParameterMissing => error
-      render json: {error: error.message }, status: 422
-    end
+    top_users = $all_posts.group_by { |row| row.name }
+                              .sort_by { |key, value| value.count}
+                              .map{ |row| {name: row.first, count: row.last.count}}
+    render json: top_users.reverse.take(10)
   end
 
   def top_ten_rooms
-    begin
-      top_ten_rooms = $all_posts.group_by{ |row| row.room }
-                                .sort_by{ |key, value| value.count }
-                                .reverse.take(10)
-                                .map { |row| {room: row.first, posts: row.last.count} }
-      render json: top_ten_rooms.take(10)
-    rescue ActionController::ParameterMissing => error
-      render json: {error: error.message }, status: 422
-    end
+    top_ten_rooms = $all_posts.group_by{ |row| row.room }
+                              .sort_by{ |key, value| value.count }
+                              .reverse.take(10)
+                              .map { |row| {room: row.first, posts: row.last.count} }
+    render json: top_ten_rooms.take(10)
   end
 
   def all_users
-    begin
-      every_user =
-      $all_posts.group_by { |row| row.name}
-      .map { |row| row.first }
-      render json: every_user
-    rescue ActionController::ParameterMissing => error
-      render json: {error: error.message }, status: 422
-    end
+    every_user =
+    $all_posts.group_by { |row| row.name}
+    .map { |row| row.first }
+    render json: every_user
   end
 
   def profile
-    begin
-      user_profile_data = Message.where(name: params.fetch(:name))
-      user_profile_data
-      render json: user_profile_data
-    rescue ActionController::ParameterMissing => error
-      render json: {error: error.message }, status: 422
-    end
+    user_profile_data = Message.where(name: params.fetch(:name))
+    user_profile_data
+    render json: user_profile_data
   end
 end
